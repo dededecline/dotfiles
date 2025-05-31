5dcbf9076036f8fafc7e97a74a454dc36692f106
Dani Klein <dani@dani.cloud>
2025-05-30 20:34:50 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -e
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+template_file="$script_dir/home/.chezmoiscripts/darwin/run_onchange_before_install-packages.sh.tmpl"
 
 # macOS Setup
 # Installs homebrew, xcode developer tools, and rosetta 2
@@ -17,13 +18,21 @@ if [ "$(uname)" = "Darwin" ]; then
   fi
   if [ ! "$(command -v chezmoi)" ]; then
     /opt/homebrew/bin/brew install chezmoi
-    template_file="$script_dir/home/.chezmoiscripts/darwin/run_onchange_before_install-packages.sh.tmpl"
     if ! grep -q '"chezmoi"' "$template_file"; then
       sed -i '' '/{{ \$brews := list/a\
      "chezmoi"' "$template_file"
     fi
   fi
+  if [ ! "$(command -v mas)" ]; then
+    /opt/homebrew/bin/brew install mas
+    if ! grep -q '"mas"' "$template_file"; then
+      sed -i '' '/{{ \$brews := list/a\
+     "mas"' "$template_file"
+    fi
+  fi
 fi
+
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc && source ~/.zshrc
 
 # exec: replace current process with chezmoi init
 exec /opt/homebrew/bin/chezmoi init --apply "--source=$script_dir"
